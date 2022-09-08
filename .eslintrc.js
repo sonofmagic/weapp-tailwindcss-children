8842bc6b2371ee5a85423572849508aa7337fdb5
ice breaker <1324318532@qq.com>
2022-09-08 20:36:08 +0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: false,
+  env: {
+    es2021: true,
+    node: true,
+    'jest/globals': true
+  },
+  extends: ['standard'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'jest'],
+  rules: {}
+}
